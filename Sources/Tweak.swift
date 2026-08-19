ce400e79366b5398771fa6478a93e3cabc1a47ea
Asasec Media <158528545+asasec@users.noreply.github.com>
2026-08-19 12:54:11 +0300

    Update Tweak.swift

--- Sources/Tweak.swift
+++ Sources/Tweak.swift
@@ -1,10 +1,7 @@
 import Foundation
 import Jinx
 
-@_cdecl("jinx_entry")
+@_cdecl("tweak_entry")
 public func jinxEntry() {
     NSLog("[JinxSwiftTweak] Tweak loaded successfully!")
-    
-    // Örnek Hook Tanımlaması (Jinx kullanarak)
-    // Hedef sınıf ve metotları buraya ekleyebilirsiniz.
 }
