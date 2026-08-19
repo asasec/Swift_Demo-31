4cb25aef6e026c9d59f522e0e45ffd6e6fba1b08
Asasec Media <158528545+asasec@users.noreply.github.com>
2026-08-19 12:17:41 +0300

    Create RootListController.swift

---
+++ prefs/RootListController.swift
@@ -0,0 +1,17 @@
+import UIKit
+import Preferences
+
+class RootListController: HBRootListController {
+    override var specifiers: NSMutableArray? {
+        get {
+            if let specifiers = _specifiers {
+                return specifiers
+            }
+            _specifiers = self.loadSpecifiersFromPlistName("Root", target: self)
+            return _specifiers
+        }
+        set {
+            super.specifiers = newValue
+        }
+    }
+}
